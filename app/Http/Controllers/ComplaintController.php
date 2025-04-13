0d1088df59fa823a2206b72a749202a6021228fb
Garry Cacho <gc120978levelup1@gmail.com>
2025-04-13 18:41:45 +0800

    af56dsg64r5

--- app/Http/Controllers/ComplaintController.php
+++ app/Http/Controllers/ComplaintController.php
@@ -56,7 +56,7 @@ class ComplaintController extends Controller
                 //'picture' => $_ENV['AWS_URL'] . "/" . Storage::disk('s3')->put('images', $request->file('image_file')),
                 //'picture' =>  $_ENV['AWS_URL'] . "/" . Storage::disk('s3')->put('images', $request->file('image_file'), 'public'),
                 //'picture' =>  $_ENV['AWS_URL'] . "/" . Storage::disk('gdisk01')->put('images', $request->file('image_file')),
-                'picture' =>  Env::$AWS_URL . "/" . Storage::disk('gdisk01')->put('images', $request->file('image_file')),
+                'picture' =>  AWS_URL . "/" . Storage::disk('gdisk01')->put('images', $request->file('image_file')),
             ]);
         }
         $complaint = Complaint::create($request->all());
