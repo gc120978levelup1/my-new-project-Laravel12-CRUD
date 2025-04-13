6b099ba461160eede8d538fbb463f70831b74c44
Garry Cacho <gc120978levelup1@gmail.com>
2025-04-13 18:50:06 +0800

    af56dsg64r5

--- alphaCloudSite/Env.php
+++ alphaCloudSite/Env.php
@@ -1,3 +1,3 @@
 <?php
 
-const AWS_URL = 'https://fls-9eaa2509-0ce6-4f12-a40a-e4d4a34152c3.laravel.cloud';
+const AWS_URL = 'https://fls-9eaa2509-0ce6-4f12-a40a-e4d4a34152c3.laravel.cloud/';
--- app/Http/Controllers/ComplaintController.php
+++ app/Http/Controllers/ComplaintController.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use alphaCloudsit\Env;
+const AWS_URL = 'https://fls-9eaa2509-0ce6-4f12-a40a-e4d4a34152c3.laravel.cloud/';
+
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use Inertia\Inertia;
