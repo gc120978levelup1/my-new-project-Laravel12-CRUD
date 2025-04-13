c20b83f787621cd0993fd45a780aab29e5abb145
Garry Cacho <gc120978levelup1@gmail.com>
2025-04-13 19:03:38 +0800

    af56dsg64r5

--- alphaCloudSite/Env.php
+++ alphaCloudSite/Env.php
@@ -1,3 +1,4 @@
 <?php
-
-const AWS_URL = 'https://fls-9eaa2509-0ce6-4f12-a40a-e4d4a34152c3.laravel.cloud/';
+return [
+ 'AWS_URL' => 'https://fls-9eaa2509-0ce6-4f12-a40a-e4d4a34152c3.laravel.cloud/',
+];
