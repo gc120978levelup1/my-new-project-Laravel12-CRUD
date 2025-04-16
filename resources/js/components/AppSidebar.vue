d8334df9c2c373894f671968c58d8847d7c41e6f
Garry M. Cacho <gc120978levelup1@gmail.com>
2025-04-16 11:42:11 +0800

    Update AppSidebar.vue

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -41,7 +41,7 @@ const footerNavItems: NavItem[] = [
             <SidebarMenu>
                 <SidebarMenuItem>
                     <SidebarMenuButton size="lg" as-child>
-                        <Link :href="route('dashboard')">
+                        <Link :href="route('home')">
                             <AppLogo />
                         </Link>
                     </SidebarMenuButton>
